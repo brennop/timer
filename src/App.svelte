81d409d64f2834252841c18eae3fd4f647ab993e
brennop <brennopcordeiro@gmail.com>
2020-04-11 22:14:02 -0300

    ♻ Format code using prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import "typeface-roboto-mono";
-  import Scrambo from "scrambo"
+  import Scrambo from "scrambo";
 
   const scrambo = new Scrambo();
   let scramble = scrambo.get();
 
-  let times = localStorage.getItem('times') || [];
+  let times = localStorage.getItem("times") || [];
 
   let precision = 2;
   let timerID = 0;
@@ -16,121 +16,103 @@
   let thresholdTimer = 0;
 
   function runTimer() {
-    const current = new Date()
+    const current = new Date();
     timer = new Date(current - startTime);
-    timerID = setTimeout(runTimer, 100)
+    timerID = setTimeout(runTimer, 100);
   }
 
   function startTimer() {
-      if(thresholdTimer === 0) {
-        if(!timerID) {
-          thresholdTimer = new Date();
-        } else {
-          // stop timer
-          const current = new Date()
-          timer = new Date(current - startTime);
-          times = [...times, { time: timer }]
-          clearTimeout(timerID);
-          timerID = 0;
-          precision = 2;
-          scramble = scrambo.get()
-        }
+    if (thresholdTimer === 0) {
+      if (!timerID) {
+        thresholdTimer = new Date();
       } else {
-        const now = new Date();
-        if(now - thresholdTimer > threshold) {
-          timer = new Date(0);
-        }
+        // stop timer
+        const current = new Date();
+        timer = new Date(current - startTime);
+        times = [...times, { time: timer }];
+        clearTimeout(timerID);
+        timerID = 0;
+        precision = 2;
+        scramble = scrambo.get();
       }
-  }
-
-  function stopTimer() {
-      if(thresholdTimer !== 0) {
-        const now = new Date();
-        if(now - thresholdTimer > threshold) {
-          precision = 1;
-          thresholdTimer = 0;
-          startTime = new Date();
-          runTimer()
-        } else {
-          thresholdTimer = 0;
-        }
+    } else {
+      const now = new Date();
+      if (now - thresholdTimer > threshold) {
+        timer = new Date(0);
       }
-  }
-
-  function handleKeydown({key}) {
-    if(key === ' ') {
-      startTimer()
     }
   }
 
-  function handleKeyup({key}) {
-    if(key === ' ') {
-      stopTimer()
+  function stopTimer() {
+    if (thresholdTimer !== 0) {
+      const now = new Date();
+      if (now - thresholdTimer > threshold) {
+        precision = 1;
+        thresholdTimer = 0;
+        startTime = new Date();
+        runTimer();
+      } else {
+        thresholdTimer = 0;
+      }
+    }
+  }
+
+  function handleKeydown({ key }) {
+    if (key === " ") {
+      startTimer();
+    }
+  }
+
+  function handleKeyup({ key }) {
+    if (key === " ") {
+      stopTimer();
     }
   }
 
   function formatTime(time, precision) {
     const seconds = time.getSeconds();
     const milliseconds = time.getMilliseconds();
-    
+
     return `${seconds}${(milliseconds / 1000).toFixed(precision).substr(1)}`;
   }
 
   function getPB(times) {
     if (times.length === 0) {
-      return '-'
+      return "-";
     } else {
-      const pb = times.reduce((prev, curr) => curr.time < prev ? curr.time : prev , Infinity);
+      const pb = times.reduce(
+        (prev, curr) => (curr.time < prev ? curr.time : prev),
+        Infinity
+      );
       return formatTime(pb, 3);
     }
   }
-
 </script>
-
-<svelte:window on:keypress={handleKeydown} on:keyup={handleKeyup}/>
-
-<main>
-  <scramble>{scramble}</scramble>
-  <timer class:dim={thresholdTimer}>{formatTime(timer, precision)}</timer>
-  <bottom>
-    <values>
-      <value>
-        <name>PB</name>
-        {getPB(times)}
-      </value>
-      <value>
-      <name>ao5</name>
-      -
-      </value>
-    </values>
-    <history></history>
-  </bottom>
-</main>
 
 <style>
   * {
     box-sizing: border-box;
   }
 
-	:global(body) {
+  :global(body) {
     padding: 0;
-	}
+  }
 
-	main {
+  main {
     font-family: Roboto Mono, monospace;
     margin: auto;
-		padding: 1em;
-  
+    padding: 1em;
+
     height: 100%;
     display: flex;
     align-items: center;
     flex-direction: column;
     justify-content: space-between;
-	}
+  }
 
-	timer {
-		font-size: 5em;
-	}
+  timer {
+    font-size: 5em;
+  }
 
   .dim {
     color: #868686;
@@ -159,3 +141,23 @@
   history {
   }
 </style>
+
+<svelte:window on:keypress={handleKeydown} on:keyup={handleKeyup} />
+
+<main>
+  <scramble>{scramble}</scramble>
+  <timer class:dim={thresholdTimer}>{formatTime(timer, precision)}</timer>
+  <bottom>
+    <values>
+      <value>
+        <name>PB</name>
+        {getPB(times)}
+      </value>
+      <value>
+        <name>ao5</name>
+        -
+      </value>
+    </values>
+    <history />
+  </bottom>
+</main>
